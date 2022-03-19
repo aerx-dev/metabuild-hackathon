6c77ed76dacd78089fb961c71cfbdd032d4bdf7b
3lLobo <flocwolf@gmail.com>
2022-03-19 18:30:20 +0100

    Charge boilerplate

--- contracts/aerx_nft/src/lib.rs
+++ contracts/aerx_nft/src/lib.rs
@@ -21,7 +21,7 @@ use near_contract_standards::non_fungible_token::metadata::{
 use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LazyOption;
+use near_sdk::collections::{LazyOption, LookupMap};
 use near_sdk::json_types::ValidAccountId;
 use near_sdk::{
     env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
@@ -36,7 +36,10 @@ near_sdk::setup_alloc!();
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
-    nft_count: Counter
+    nft_count: Counter,
+    // TODO: is this correct?
+    commentMap: LookupMap<u8, Vec<String>>,
+    chargeMap: LookupMap<u8, u128>,
 }
 
 const MATE_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath fill='%23C4A2FC' d='M269.9 130.8c-20.5-42.4-51.3-42.3-71.7 0-8.6 18.2-13.2 38.1-13.6 58.2 0 23.7 16.9 44.1 40.1 48.6-6.1 18-18 33.7-40.8 37-2.5.4-4.2 2.6-3.8 5.1s2.6 4.2 5.1 3.8c23.9-3.4 40.5-18.9 48.8-45 5.9-20.7 7.5-42.3 4.6-63.6-.4-2.5 1.3-4.7 3.8-5.1 2.5-.4 4.7 1.3 5.1 3.8 2.8 21.4 1.5 43.1-3.8 64 23.2-4.6 39.9-24.9 39.9-48.6-.5-20.1-5.1-40-13.7-58.2zM170.4 99c-23.9 3.4-40.5 18.9-48.8 45-5.8 20.7-7.3 42.3-4.6 63.6.4 2.5-1.3 4.7-3.8 5.1-2.5.4-4.7-1.3-5.1-3.8-2.8-21.4-1.5-43.1 3.8-64-23.1 4.6-39.9 25-39.9 48.6.4 20.1 5 40 13.6 58.2 20.5 42.4 51.3 42.3 71.7 0 8.6-18.2 13.2-38.1 13.6-58.2 0-23.7-16.9-44.1-40.1-48.6 6.1-18 18-33.7 40.8-37 2.5-.4 4.2-2.6 3.8-5.1s-2.6-4.1-5-3.8z'/%3E%3Cpath fill='%236D2ED3' d='M189 202.5h-45.5c25.8-12 44.8-35 51.7-62.6s1-56.8-16.1-79.6c-.9-1.1-2.2-1.8-3.6-1.8h-42.2l18.3-36.6 38.4-8.5c1.6-.3 2.9-1.5 3.3-3 .5-1.5.1-3.2-1-4.4-1.1-1.2-2.7-1.7-4.3-1.3l-40.5 9c-1.3.3-2.4 1.2-3 2.4l-21.3 42.5H31.5c-1.4 0-2.7.7-3.6 1.8-17.1 22.8-23 52-16.1 79.6s25.9 50.6 51.7 62.6H18c-7.5 0-13.5 6-13.5 13.5s6 13.5 13.5 13.5h171c7.5 0 13.5-6 13.5-13.5 0-7.6-6-13.6-13.5-13.6zM33.8 67.5h139.4c3 4.3 5.6 8.8 7.8 13.5H26c2.2-4.7 4.8-9.3 7.8-13.5zM18 117c0-9.2 1.5-18.3 4.4-27h162.3c2.9 8.7 4.4 17.8 4.4 27 0 1.4-.1 2.8-.2 4.2l-21.2 8.9-19.7-8.3c-1.1-.5-2.4-.5-3.5 0l-19.6 8.3-19.6-8.3c-1.1-.5-2.4-.5-3.5 0l-19.6 8.3-19.6-8.3c-1.1-.5-2.4-.5-3.5 0l-19.6 8.3-21.2-8.9c-.2-1.4-.3-2.8-.3-4.2zm1.3 14.4 18.3 7.7c1.1.5 2.4.5 3.5 0l19.6-8.3 19.6 8.3c1.1.5 2.4.5 3.5 0l19.6-8.3 19.6 8.3c1.1.5 2.4.5 3.5 0l19.6-8.3 19.7 8.3c1.1.5 2.4.5 3.5 0l18.3-7.7c-1.1 6.8-3.1 13.4-5.8 19.7l-14.2 6-19.7-8.3c-1.1-.5-2.4-.5-3.5 0l-19.6 8.3-19.6-8.3c-1.1-.5-2.4-.5-3.5 0l-19.6 8.3-19.6-8.3c-1.1-.5-2.4-.5-3.5 0l-19.6 8.3-14.2-6c-2.8-6.3-4.7-12.9-5.9-19.7zM32 163.8l5.6 2.4c1.1.5 2.4.5 3.5 0l19.6-8.3 19.6 8.3c1.1.5 2.4.5 3.5 0l19.6-8.3 19.6 8.3c1.1.5 2.4.5 3.5 0l19.6-8.3 19.7 8.3c1.1.5 2.4.5 3.5 0l5.6-2.4c-15.8 24.2-42.6 38.7-71.5 38.7-28.7 0-55.6-14.6-71.4-38.7zm157 56.7H18c-2.5 0-4.5-2-4.5-4.5s2-4.5 4.5-4.5h171c2.5 0 4.5 2 4.5 4.5s-2 4.5-4.5 4.5z'/%3E%3C/g%3E%3C/svg%3E";
@@ -85,7 +88,7 @@ impl Contract {
     pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         metadata.assert_valid();
-        let val_zero: i8 = 0;
+        let val_zero: u8 = 0;
         let nft_counter = Counter::counter_init(val_zero);
         Self {
             tokens: NonFungibleToken::new(
@@ -96,9 +99,13 @@ impl Contract {
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
-            nft_count: nft_counter
+            nft_count: nft_counter,
+            commentMap: LookupMap::new(b"comment".to_vec()),
+            chargeMap: LookupMap::new(b"charge".to_vec())
         }
     }
+
+
 
     /// Mint a new token with ID=`token_id` belonging to `receiver_id`.
     ///
@@ -116,10 +123,41 @@ impl Contract {
     ) -> Token {
         // NFT count increments with +1 everytime a NFT is minted. The tokenID is therefore incremental and unique.
         self.nft_count.increment();
-        let token_id = self.nft_count.get_num().to_string();
-        self.tokens.mint(token_id, receiver_id, Some(token_metadata))
+        let token_id: u8 = self.nft_count.get_num();
+        let token_id_str = self.nft_count.get_num().to_string();
+        self.commentMap.insert(&token_id, &vec![]);
+        self.chargeMap.insert(&token_id, &0);
+        self.tokens.mint(token_id_str, receiver_id, Some(token_metadata))
+    }
+
+    #[payable]
+    pub fn setComment( &mut self, token_id: u8, comment: String) {
+        // update the commentMap with insert
+    }
+
+    #[payable]
+    pub fn setCharge( &mut self, token_id: u8, charge: u128) {
+        // assert the caller is the contract itself
+        // update the chargeMap with insert. First get the current value, add the charge, insert.
+    }
+
+    pub fn getComments( &mut self, token_id: u8) -> Vec<String> {
+        // return all comments for that ID
+        match self.commentMap.get(&token_id){
+            Some(x)=>x, // vector that contains all memos
+            None=>vec![] //else this will return an empty vector
+        }
+    }
+
+    pub fn getCharge( &mut self, token_id: u8) -> u128 {
+        // return the charge for that ID
+        match self.chargeMap.get(&token_id){
+            Some(x) => x, // vector that contains all memos
+            None => 0 //else this will return an empty vector
+        }
     }
 }
+
 
 near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
 near_contract_standards::impl_non_fungible_token_approval!(Contract, tokens);
